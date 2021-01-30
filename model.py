40ec07ac64fe461ead85d0510f97ca3fc34e4353
sergei-karimov <s.karimov@outlook.com>
2021-01-30 22:17:58 +0200

    add data folder

---
+++ model.py
@@ -0,0 +1,13 @@
+
+import csv
+
+lines = []
+with open('data/drin=ving_log.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    for line in reader:
+        lines.append(line)
+
+images = []
+measurements = []
+for line in lines:
+    sou
