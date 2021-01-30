043ed5c4393ab8a79825ccfb0665c03d02776725
sergei-karimov <s.karimov@outlook.com>
2021-01-30 22:33:25 +0200

    add load filenames

--- model.py
+++ model.py
@@ -2,7 +2,7 @@
 import csv
 
 lines = []
-with open('data/drin=ving_log.csv') as csvfile:
+with open('data/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
         lines.append(line)
@@ -10,4 +10,5 @@ with open('data/drin=ving_log.csv') as csvfile:
 images = []
 measurements = []
 for line in lines:
-    sou
+    image_path = line[0]
+    image_file_name = image_path.split('\\')[-1]
