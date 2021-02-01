68e0416ca6d901e6e8dadef619a20e812db67821
sergei-karimov <s.karimov@outlook.com>
2021-02-01 08:47:05 +0200

    Add plot results

--- model.py
+++ model.py
@@ -3,7 +3,7 @@ import csv
 import numpy as np
 from tensorflow import keras
 from tensorflow.keras import layers
-
+import matplotlib.pyplot as plt
 
 lines = []
 with open('./data/driving_log.csv') as csvfile:
@@ -27,13 +27,9 @@ y_train = np.array(measurements)
 
 model = keras.Sequential()
 model.add(layers.Cropping2D(cropping=((50, 20), (0, 0)), input_shape=(160, 320, 3)))
-print(model.summary())
 model.add(layers.Lambda(lambda x: x / 127.5 - 1.0))
-print(model.summary())
 model.add(layers.Conv2D(24, (5, 5), strides=(2, 2), activation='relu'))
-print(model.summary())
 model.add(layers.Conv2D(36, (5, 5), strides=(2, 2), activation='relu'))
-print(model.summary())
 model.add(layers.Conv2D(48, (5, 5), strides=(2, 2), activation='relu'))
 model.add(layers.Conv2D(64, (3, 3), strides=(2, 2), activation='relu'))
 model.add(layers.Conv2D(64, (3, 3), strides=(2, 2), activation='relu'))
@@ -50,5 +46,17 @@ model.add(layers.Dense(1))
 model.summary()
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, epochs=7, shuffle=True)
+history_object = model.fit(X_train, y_train, validation_split=0.2, epochs=7, shuffle=True)
 model.save('model.h5')
+
+# print the keys contained in the history object
+print(history_object.history.keys())
+
+# plot the training and validation loss for each epoch
+plt.plot(history_object.history['loss'])
+plt.plot(history_object.history['val_loss'])
+plt.title('model mean squared error loss')
+plt.ylabel('mean squared error loss')
+plt.xlabel('epoch')
+plt.legend(['training set', 'validation set'], loc='upper right')
+plt.show()
